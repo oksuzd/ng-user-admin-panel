fed18210ad4db2a700387c96910bb4448a8578d7
oksuzd <oksuzdal@mail.ru>
2023-06-15 09:39:50 -0300

    deleting user added

--- src/app/admin/users/grid-components/ag-row-delete/ag-row-delete.component.ts
+++ src/app/admin/users/grid-components/ag-row-delete/ag-row-delete.component.ts
@@ -2,24 +2,22 @@ import { Component } from '@angular/core';
 import { ICellRendererAngularComp } from "ag-grid-angular";
 import { ICellRendererParams } from "ag-grid-community";
 import { UserCellsParams } from "../../models/user-grid.model";
+import { MatDialog } from "@angular/material/dialog";
+import { DialogWindowComponent } from "../../../../shared/components/dialog-window/dialog-window.component";
 
 @Component({
   selector: 'app-ag-row-delete',
   templateUrl: './ag-row-delete.component.html',
   styleUrls: ['./ag-row-delete.component.scss']
 })
-export class AgRowDeleteComponent implements ICellRendererAngularComp{
+export class AgRowDeleteComponent implements ICellRendererAngularComp {
 
-  value = 'X';
-  userId!: number | undefined;
-  // value!: number | undefined;
-  // userId: number | undefined;
+  params!: ICellRendererParams & UserCellsParams;
+
+  constructor(public dialog: MatDialog) {}
 
   agInit(params: ICellRendererParams & UserCellsParams): void {
-    // this.value = 0;
-    // this.value = params.value;
-    // this.userId = params.userId;
-    this.userId = params.value;
+    this.params = params;
   }
 
   refresh(params: ICellRendererParams<any>): boolean {
@@ -27,7 +25,12 @@ export class AgRowDeleteComponent implements ICellRendererAngularComp{
   }
 
   deleteUser() {
-    // this.value = this.userId;
-    const delUserId = this.userId;
+    const dialogRef = this.dialog.open(DialogWindowComponent, {width: '250px'});
+    dialogRef.afterClosed()
+      .subscribe((res) => {
+        if (!!res) {
+          this.params.onDelete(this.params.data);
+        }
+      })
   }
 }
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,14 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { DialogWindowComponent } from "./components/dialog-window/dialog-window.component";
+
+
 import { MatButtonModule } from "@angular/material/button";
+import { MatDialogModule } from "@angular/material/dialog";
 
 const materialModules = [
   MatButtonModule,
+  MatDialogModule,
 ];
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    DialogWindowComponent,
+  ],
   imports: [
     CommonModule,
     ...materialModules,
@@ -17,4 +24,5 @@ const materialModules = [
     ...materialModules,
   ]
 })
-export class SharedModule { }
+export class SharedModule {
+}
